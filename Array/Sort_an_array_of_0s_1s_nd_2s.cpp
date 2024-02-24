c6b69ef80505ac5d14e2281eee8db0380ffd069d
Subha Mondal <subha.snt2002@gmail.com>
2024-02-24 11:56:18 +0530

    Sort an array of 0s, 1s and 2s

---
+++ Array/Sort_an_array_of_0s_1s_nd_2s.cpp
@@ -0,0 +1,53 @@
+// STRIVER
+
+// Sort an array of 0s, 1s and 2s
+// Problem Statement: Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+
+// Examples
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+
+// Input: nums = [0]
+// Output: [0]
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(vector<int>& arr, int n) {
+
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main()
+{
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
